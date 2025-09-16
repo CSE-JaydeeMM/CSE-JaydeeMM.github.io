5df284cd2b3f1c1b077e9abe7b4b0dada28e95d4
CSE-JaydeeMM <Jaydee.mal05@icloud.com>
2025-09-16 12:56:55 -0400

    here

--- csce242/examples/Zip/js/intro.js
+++ csce242/examples/Zip/js/intro.js
@@ -7,26 +7,26 @@
 const btnClickMe = document.getElementById("btn-click-me").oneclick = sayHello;
 */
 
-document.getElementById("btn-click-me").oneclick = (event) => {
+document.getElementById("btn-click-me").onclick = (event) => {
 document.getElementById("p-welcome").innerHTML = ("Hello World");
 event.currentTarget.classList.add("clicked"); //current target is the button that was clicked 
 
 };
 
-document.getElementById("btn-happy").oneclick = () => {
+document.getElementById("btn-happy").onclick = () => {
     const pFeeling = document.getElementById("p-feeling");
     pFeeling.innerHTML = "YAY"
     pFeeling.classList.add("happy");
     pFeeling.classList.remove("sad")
 
 }
-document.getElementById("btn-sad").oneclick = () => {
+document.getElementById("btn-sad").onclick = () => {
     const pFeeling = document.getElementById("p-feeling");
     pFeeling.innerHTML = "Nay"
     pFeeling.classList.add("sad");
     pFeeling.classList.remove("happy")
 }
-document.getElementById("btn-clear").oneclick = () => {
+document.getElementById("btn-clear").onclick = () => {
     const pFeeling = document.getElementById("p-feeling");
     pFeeling.innerHTML = ""
     pFeeling.classList.remove("happy");
@@ -38,3 +38,41 @@ const userInput = event.currentTarget.value;
 document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}`
 document.getElementById("img-emotion").classList.remove("hidden");
 };
+
+document.getElementById("btn-mood-ring").onclick = () =>{
+    const color = document.getElementById("text-color").value.trim().toLowerCase();
+    const p = document.getElementById("p-mood-results");
+    p.innerHTML = "";
+    const error = document.getElementById("error-color");
+    error.innerHTML = "";
+    let mood = "";
+
+
+    if(color == ""){
+         error.innerHTML = "blank";
+        return;
+    }
+
+    if(color == "red"){
+        mood= "angry";
+    } else if (color == "yellow") {
+        mood = "mellow";
+    } 
+
+    if( mood = ""){
+        error.innerHTML = "invalid color";
+        return;
+    }
+    p.innerHTML = `Your choose ${color}, so you are feeling ${mood}`;
+}
+
+document.getElementById("btn-bounce").onclick = (event) => {
+    const ball = document.getElementById("ball");
+
+    if(ball.classList.currentTarget("bounce")){
+        event.currentTarget("Start")
+    } else  {
+        event.currentTarget.innerHTML = "Stop";
+    }
+    ball.classList.toggle("bounce");
+}
